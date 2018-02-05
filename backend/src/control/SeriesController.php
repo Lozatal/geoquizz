c2b4d958855fed8d2d0e15f81d84aba64c22abbe
Gerardo RAZO JABANA <mossbi.prog@gmail.com>
2018-02-05 15:25:53 +0100

    test

--- backend/src/control/SeriesController.php
+++ backend/src/control/SeriesController.php
@@ -5,20 +5,42 @@
   use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
 
-  use geoquizz\model\Compte as compte;
+  use geoquizz\model\Serie as serie;
+
+  use lbs\utils\Writer as writer;
+  use lbs\utils\Pagination as pagination;
 
   use illuminate\database\Eloquent\ModelNotFoundException as ModelNotFoundException;
 
-  class ComptesControlleur{
-
+  class SeriesController{
     public $conteneur=null;
-
     public function __construct($conteneur){
       $this->conteneur=$conteneur;
     }
 
-    public function postCompte(Request $req,Response $resp,array $args){
-      $photos=photos::get();
+    /*
+    * Retourne la liste des Sandwichs, avec filtre et pagination
+    * @param : Request $req, Response $resp, array $args[]
+    * Return Response $resp contenant la page complète
+    */
+    public function getSeries(Request $req,Response $resp,array $args){
+/*
+      $type = $req->getQueryParam('type',null);
+      $img = $req->getQueryParam('img',null);
+*/
+      $size = $req->getQueryParam('size',10);
+      $page = $req->getQueryParam('page',1);
+
+      $q = serie::select('*');
+
+      //Récupération du total d'élement de la recherche
+      $total = sizeof($q->get());
+
+      $returnPag=pagination::page($q,$size,$page,$total);
+      $listeSeries = $returnPag["request"]->get();
+
+      $tab = writer::addLink($listeSeries, 'series', 'seriesListe');
+      $json = writer::jsonFormatCollection("series",$tab,$total,$size,$returnPag["page"]);
 
       $resp=$resp->withHeader('Content-Type','application/json');
       $resp->getBody()->write($json);
--- backend/src/model/Serie.php
+++ backend/src/model/Serie.php
@@ -1,20 +1,10 @@
 <?php
+
 namespace geoquizz\model;
 
 class Serie extends \Illuminate\Database\Eloquent\Model {
 
-       protected $table      = 'serie';
-       protected $primaryKey = 'id';
-       public $incrementing = false;
-       public $keyType = 'string';
-       public $timestamps = false;
-
-       public function photos(){
-         return $this->hasMany( 'lbs\model\Photos', 'id_serie');
-       }
-
-       public function parties(){
-         return $this->hasMany( 'lbs\model\Partie', 'id_serie');
-       }
-
+  protected $table = 'serie';
+  protected $primaryKey = 'id';
+  public $timestamps = false;
 }
