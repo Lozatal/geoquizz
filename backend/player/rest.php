c9653bea953f92fc768e8863b340e8679da8520d
daniel <db.daniel.bentz@gmail.com>
2018-02-05 13:47:58 +0100

    Ajout partiesController

---
+++ backend/player/rest.php
@@ -0,0 +1,77 @@
+<?php
+  require_once __DIR__ . '/../src/vendor/autoload.php';
+  use \Psr\Http\Message\ServerRequestInterface as Request;
+  use \Psr\Http\Message\ResponseInterface as Response;
+  use \DavidePastore\Slim\Validation\Validation as Validation;
+  use \Respect\Validation\Validator as Validator;
+  use illuminate\database\Eloquent\ModelNotFoundException as ModelNotFoundException;
+
+  /* Appel des contrôleurs */
+
+  use \geoquizz\control\PartiesController as Parties;
+
+  /* Appel des utilitaires */
+
+  use \geoquizz\utils\Writer as writer;
+
+  $config=parse_ini_file("../src/config/geoquizz.db.conf.ini");
+  $db = new Illuminate\Database\Capsule\Manager();
+  $db->addConnection($config);
+  $db->setAsGlobal();
+  $db->bootEloquent();
+
+  //Création et configuration du container
+  $configuration=[
+    'settings'=>[
+      'displayErrorDetails'=>true,
+      'production' => false
+    ]
+  ];
+
+  $errors = require_once __DIR__ . '/../src/config/api_errors.php';
+
+  $c=new \Slim\Container(array_merge( $configuration, $errors) );
+  $app=new \Slim\App($c);
+  $c = $app->getContainer();
+
+  //Initialisation du conteneur pour le writer
+  new writer($c);
+
+  //Application
+
+  function checkToken(Request $rq, Response $rs, callable $next){
+    // récupérer l'identifiant de commde dans la route et le token
+    $id = $rq->getAttribute('route')->getArgument( 'id');
+    $token = $rq->getQueryParam('token', null);
+    // vérifier que le token correspond à la commande
+    try
+    {
+        ModelCommande::where('id', '=', $id)->where('token', '=',$token)->firstOrFail();
+    } catch (ModelNotFoundException $e) {
+        $rs= $rs->withStatus(404);
+        $temp = array("type" => "error", "error" => '404', "message" => "Le token n'est pas valide");
+        $rs->getBody()->write(json_encode($temp));
+        return $rs;
+    };
+    return $next($rq, $rs);
+  };
+
+  function afficheError(Response $resp, $location, $errors){
+  	$resp=$resp->withHeader('Content-Type','application/json')
+  	->withStatus(400)
+  	->withHeader('Location', $location);
+  	$resp->getBody()->write(json_encode($errors));
+  	return $resp;
+  }
+
+  //Parties
+
+  $app->get('/parties[/]',
+    function(Request $req, Response $resp, $args){
+      $ctrl=new Parties($this);
+      return $ctrl->getParties($req,$resp,$args);
+    }
+  )->setName("partiesListe");
+
+  $app->run();
+?>
---
+++ backend/src/control/PartiesController.php
@@ -0,0 +1,31 @@
+<?php
+
+  namespace geoquizz\control;
+
+  use \Psr\Http\Message\ServerRequestInterface as Request;
+  use \Psr\Http\Message\ResponseInterface as Response;
+
+  use geoquizz\model\Parties as parties;
+
+  use illuminate\database\Eloquent\ModelNotFoundException as ModelNotFoundException;
+
+  class PartiesControlleur{
+    public $conteneur=null;
+    public function __construct($conteneur){
+      $this->conteneur=$conteneur;
+    }
+
+    /*
+    * Retourne la page de modification d'un sandwich
+    * @param : Request $req, Response $resp, array $args[]
+    * Return Response $resp contenant la page complète
+    */
+    public function getParties(Request $req,Response $resp,array $args){
+      $photos=parties::get();
+
+      $resp=$resp->withHeader('Content-Type','application/json');
+      $resp->getBody()->write($photos);
+      return $resp;
+    }
+
+  }
