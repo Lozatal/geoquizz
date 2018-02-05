50d4a6e428b501ffe0bb3c5da796c249f2526397
daniel <db.daniel.bentz@gmail.com>
2018-02-05 13:27:19 +0100

    Ajout vue-cli

---
+++ player/src/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+	state : {
+		token : false,
+	},
+	mutations : {
+		setToken(state,token) {
+			state.token = token;
+		},
+		initialiseStore(state) {
+			if(localStorage.getItem('store')) {
+				this.replaceState(
+					Object.assign(state, JSON.parse(localStorage.getItem('store')))
+				);
+			}
+		}
+	}
+})
