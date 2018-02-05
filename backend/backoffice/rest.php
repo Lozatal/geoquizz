f7f965b477b0bde979bc447fd1f389e976ca73a1
Ragonneau <v.ragonneau@laposte.net>
2018-02-05 13:26:29 +0100

    rest route

---
+++ backend/backoffice/rest.php
@@ -0,0 +1,80 @@
+<?php
+  require_once __DIR__ . '/../src/vendor/autoload.php';
+  use \Psr\Http\Message\ServerRequestInterface as Request;
+  use \Psr\Http\Message\ResponseInterface as Response;
+  use \DavidePastore\Slim\Validation\Validation as Validation;
+  use \Respect\Validation\Validator as Validator;
+  use illuminate\database\Eloquent\ModelNotFoundException as ModelNotFoundException;
+
+  /* Appel des contrôleurs */
+
+
+  use \geoquizz\control\publique\PhotosController as Photos;
+  use \geoquizz\control\publique\ComptesController as Comptes;
+
+  /* Appel des utilitaires */
+
+  use \geoquizz\utils\Writer as writer;
+
+
+  $config=parse_ini_file("../src/config/geoquizz.db.conf.ini");
+  $db = new Illuminate\Database\Capsule\Manager();
+  $db->addConnection($config);
+  $db->setAsGlobal();
+  $db->bootEloquent();
+
+  //Création et configuration du container
+  $configuration=[
+    'settings'=>[
+      'displayErrorDetails'=>true,
+      'production' => false
+    ]
+  ];
+
+  $errors = require_once __DIR__ . '/../src/config/api_errors.php';
+
+  $c=new \Slim\Container(array_merge( $configuration, $errors) );
+  $app=new \Slim\App($c);
+  $c = $app->getContainer();
+
+  //Initialisation du conteneur pour le writer
+  new writer($c);
+
+  //Application
+
+  function checkToken(Request $rq, Response $rs, callable $next){
+    // récupérer l'identifiant de commde dans la route et le token
+    $id = $rq->getAttribute('route')->getArgument( 'id');
+    $token = $rq->getQueryParam('token', null);
+    // vérifier que le token correspond à la commande
+    try
+    {
+        ModelCommande::where('id', '=', $id)->where('token', '=',$token)->firstOrFail();
+    } catch (ModelNotFoundException $e) {
+        $rs= $rs->withStatus(404);
+        $temp = array("type" => "error", "error" => '404', "message" => "Le token n'est pas valide");
+        $rs->getBody()->write(json_encode($temp));
+        return $rs;
+    };
+    return $next($rq, $rs);
+  };
+
+  function afficheError(Response $resp, $location, $errors){
+  	$resp=$resp->withHeader('Content-Type','application/json')
+  	->withStatus(400)
+  	->withHeader('Location', $location);
+  	$resp->getBody()->write(json_encode($errors));
+  	return $resp;
+  }
+
+  //Photos
+
+  $app->get('/photos[/]',
+    function(Request $req, Response $resp, $args){
+      $ctrl=new Photos($this);
+      return $ctrl->getPhotos($req,$resp,$args);
+    }
+  )->setName("photosListe");
+
+  $app->run();
+?>
