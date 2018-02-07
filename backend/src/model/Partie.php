2da337186cfabb43a2f9a18cb22b18079ea4a100
daniel <db.daniel.bentz@gmail.com>
2018-02-07 18:05:49 +0100

    Commit passage de index a partie

--- backend/src/model/Partie.php
+++ backend/src/model/Partie.php
@@ -6,7 +6,9 @@ class Partie extends \Illuminate\Database\Eloquent\Model {
 
   protected $table = 'partie';
   protected $primaryKey = 'id';
+  protected $keyType = 'string';
   public $timestamps = false;
+  public $incrementing = false;
 
   public function serie(){
   	return $this->belongsTo('geoquizz\model\Serie', 'id_serie');
