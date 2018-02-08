c79552db4014def73e766b3abcce1757452abd4c
GerardoGtz25 <ge.gut5@gmail.com>
2018-02-08 10:25:30 +0100

    Javascript

--- backend/backoffice/style/js/map.js
+++ backend/backoffice/style/js/map.js
@@ -1,26 +1,20 @@
 function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
-
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: myLatLng
+    center: {lat: 48.8574100, lng: 2.3338000},
+    draggable: true,
+    zoom: 10
   });
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
+  map.addListener('click', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
   });
 }
-      // Attaches an info window to a marker with the provided message. When the
-      // marker is clicked, the info window will open with the secret message.
-      function attachSecretMessage(marker, secretMessage) {
-        var infowindow = new google.maps.InfoWindow({
-          content: secretMessage
-        });
 
-        marker.addListener('click', function() {
-          infowindow.open(marker.get('map'), marker);
-        });
-      }
+function placeMarkerAndPanTo(latLng, map) {
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+
+}
  // AIzaSyCXalgdg7BmoXIaoUO3gZiZwiLM8_rlr5Q
