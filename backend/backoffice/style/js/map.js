529c442720a4c2fe901ef8405db0c04d29d72313
Ragonneau <v.ragonneau@laposte.net>
2018-02-08 15:39:21 +0100

    map avec marker qui fonctionne mais pas centre

--- backend/backoffice/style/js/map.js
+++ backend/backoffice/style/js/map.js
@@ -1,31 +1,55 @@
+var map;
+var markers = [];
+
 function initMap() {
+  var haightAshbury = {lat: 37.769, lng: -122.446};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 48.8574100, lng: 2.3338000},
-    draggable: false,
-    zoom: 10
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: haightAshbury,
+    mapTypeId: 'terrain'
   });
 
-  map.addListener('click', function(e) {
-    placeMarkerAndPanTo(e.latLng, map);
-  });
-
-  map.addListener('click', function(e){
-    let lat = e.latLng.lat();
-    let lng = e.latLng.lng();
+  // This event listener will call addMarker() when the map is clicked.
+  map.addListener('click', function(event) {
+    deleteMarkers();
+    addMarker(event.latLng);
+    let lat = event.latLng.lat();
+    let lng = event.latLng.lng();
     document.getElementById("position_long").value = lat;
     document.getElementById("position_lat").value = lng;
-    e.latLng.lat()=0;
-
+    PremierMarker=false;
   });
 }
 
-function placeMarkerAndPanTo(latLng, map) {
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
   var marker = new google.maps.Marker({
-  position: latLng,
-  map: map
+    position: location,
+    map: map
   });
+  markers.push(marker);
 }
 
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
 
- // AIzaSyCXalgdg7BmoXIaoUO3gZiZwiLM8_rlr5Q
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
--- backend/backoffice/style/js/map2.js
+++ backend/backoffice/style/js/map2.js
@@ -1,6 +1,9 @@
 function initMap() {
+  var map;
+  var markers = [];
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.8574100, lng: 2.3338000},
     draggable: false,
     zoom: 10
@@ -11,16 +14,59 @@ function initMap() {
   });
 
   map.addListener('click', function(e){
-    document.getElementById("map_refs").value = e.latLng;
+    let PremierMarker=true;
+    if(PremierMarker){
+      let lat = e.latLng.lat();
+      let lng = e.latLng.lng();
+      document.getElementById("position_long").value = lat;
+      document.getElementById("position_lat").value = lng;
+      PremierMarker=false;
+    }else{
+      deleteMarkers();
+      let lat = e.latLng.lat();
+      let lng = e.latLng.lng();
+      document.getElementById("position_long").value = lat;
+      document.getElementById("position_lat").value = lng;
+      PremierMarker=true;
+    }
   });
+
+  function placeMarkerAndPanTo(latLng, map) {
+    marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+
+  // Adds a marker to the map and push to the array.
+  function addMarker(location) {
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+    markers.push(marker);
+  }
+
+  // Sets the map on all markers in the array.
+  function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
+
+  // Removes the markers from the map, but keeps them in the array.
+  function clearMarkers() {
+    setMapOnAll(null);
+  }
+
+  // Shows any markers currently in the array.
+  function showMarkers() {
+    setMapOnAll(map);
+  }
+
+  // Deletes all markers in the array by removing references to them.
+  function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+  }
 }
-
-function placeMarkerAndPanTo(latLng, map) {
-  var marker = new google.maps.Marker({
-  position: latLng,
-  map: map
-  });
-}
-
-
- // AIzaSyCXalgdg7BmoXIaoUO3gZiZwiLM8_rlr5Q
